901b90869f2a1f06b5174060224712587c0fa15b
Lia Moua <moua0061@yahoo.com>
2021-10-12 17:09:19 -0400

    completed basic form

--- src/Components/TeamForm.js
+++ src/Components/TeamForm.js
@@ -4,7 +4,34 @@ export default function TeamForm(props){
 
     return (
         <form className='form container'>
-            <label>Name</label>
+            <label>Name
+                <input 
+                    type='text'
+                    name='name'
+                    maxLength='100'
+                    placeholder='enter your name'
+                />
+            </label>
+
+            <label>Email 
+                <input 
+                type='email'
+                name='email'
+                placeholder='enter your email'
+                />
+            </label>
+
+            <label>Role
+                <select name='role'>
+                    <option value=''>Select a role</option>
+                    <option value=''>Backend Engineer</option>
+                    <option value=''>Frontend Engineer</option>
+                    <option value=''>UI/UX Designer</option>
+                    <option value=''>Full-Stack Engineer</option>
+                    <option value=''>Other</option>
+                </select>
+            </label>
+            <button>Submit</button>
         </form>
-    )
-}
+
+)}
